837c94f0efae7e749d9b330e066382ecbc618e05
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-10 03:11:52 +0530

    commit

--- leetcodee/leetcode2/sortcolors.cpp
+++ leetcodee/leetcode2/sortcolors.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
+int main() {
+    vector<int> arr{2, 0, 2, 1, 1, 0};
+    int l = 0, m = 0, h = arr.size() - 1;
 
+    while (m <= h) {
+        if (arr[m] == 0) {
+            swap(arr[l], arr[m]);
+            l++;
+            m++;
+        } else if (arr[m] == 1) {
+            m++;
+        } else {
+            swap(arr[m], arr[h]);
+            h--;
+        }
+    }
 
-int main(){
-    vector<int>arr{1,0,}
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
