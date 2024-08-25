01342515bd72913a05fadea8d160715c17203637
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-25 12:28:04 +0530

    commit

--- pointers/arrayPointer.cpp
+++ pointers/arrayPointer.cpp
@@ -5,13 +5,21 @@ int main(){
    int arr[5] = { 1, 2, 3, 4, 5 }; 
     int* ptr = arr; 
   
-   cout<<arr<<endl;
-   cout<<&arr<<endl;
-   cout<<&arr[0]<<endl;
-   cout<<*arr<<endl;
-   cout<<ptr<<endl;
-   cout<<&ptr<<endl;
-   cout<<*ptr<<endl;
+//    cout<<arr<<endl;
+//    cout<<&arr<<endl;
+//    cout<<&arr[0]<<endl;
+//    cout<<*arr<<endl;
+//    cout<<ptr<<endl;
+//    cout<<&ptr<<endl;
+//    cout<<*ptr<<endl;
+
+
+   cout<<*ptr+2<<endl;
+   cout<<(*ptr)+2<<endl;
+   cout<<arr[4]<<endl;
+   cout<<*(arr+4)<<endl;
+
+  
 
     return 0;
 }
