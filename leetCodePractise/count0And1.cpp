1ad969e8c5f4ee72878e38200f53a9e36a382e6c
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-26 00:52:42 +0530

    commit

---
+++ leetCodePractise/count0And1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr{0,1,0,1,1,0,1,1};
+    int n = arr.size();
+
+    int numZeroes=0;
+    int numOnes=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            numZeroes++;
+            }
+            else{
+                numOnes++;
+                }
+
+    }
+
+    cout<<"number of zeroes :" <<numZeroes;
+    cout<<"number of ones :" <<numOnes;
+    return 0;
+}
