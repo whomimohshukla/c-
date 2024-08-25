54076034f2d504a459900d63670d3ce7da78808f
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-26 00:15:37 +0530

    commit

---
+++ basicMaths/lcm.Hcf.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int hcf(int a,int b){
+    if(b==0)
+    return a;
+    if (a==0)
+    return b;
+    while (a>0 && b>0)
+    {
+        if (a>b)
+        { 
+            a=a-b;
+        }
+        else{
+            b=b-a;
+        }   
+    }
+    return a==0?b:a;
+  
+}
+int main(){
+    int a=24;
+    int b=36;
+    int ans=hcf(a,b);
+    cout<<"The HCF of "<<a<<" and "<<b<<" is "<<ans;
+    return 0;
+}
