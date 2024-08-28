a92de440f0c27e83ab9b649e2d44b3f7fe9af737
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-29 00:44:01 +0530

    commit

---
+++ recursion/climbLadder.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int howToClimb(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    return howToClimb(n-1)+howToClimb(n-2);
+}
+int main(){
+    cout<<"Enter a number:";
+    int n;
+    cin>>n;
+
+    int ans=howToClimb(n);
+    cout<<"Number of ways to climb "<<n<<" stairs is "<<ans;
+
+    return 0;
+}
