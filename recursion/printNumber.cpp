00b41ac386f0c2247aab1ced26242c70ac677dc8
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-30 01:51:36 +0530

    commit

---
+++ recursion/printNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void printNumber(int num){
+    if (num==0)
+    {
+        return;
+    }
+
+    int number=
+    
+
+    int newValue=num/10;
+
+    return printNumber(newValue)
+    
+
+}
+int main(){
+    int num=647;
+    printNumber(num);
+    return 0;
+}
