0d295dc4818e2fa575536b29114050f3c3fb9f87
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-30 02:00:55 +0530

    commit

--- recursion/printNumber.cpp
+++ recursion/printNumber.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 using namespace std;
-void printNumber(int num){
-    if (num==0)
-    {
+
+void printNumber(int num) {
+    if (num == 0) {
         return;
     }
 
-    int number=num%10;
-    cout<<number;
-    
+    int newvalue = num / 10;
+    printNumber(newvalue);  // Recursive call with the updated value
 
-    int newValue=num/10;
-
-    return printNumber(newValue)
-    
-
+    int value = num % 10;
+    cout << value <<" ";  // Print the last digit after the recursive call
 }
-int main(){
-    int num=647;
+
+int main() {
+    int num = 647;
     printNumber(num);
     return 0;
 }
