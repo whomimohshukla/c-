55ce8dceb504449b745226f191f44a8f7f54bf6e
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-30 18:05:14 +0530

    commit

---
+++ recursion/lastOccuranceOfString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void lastOccurance(string &str, char target, int index, int &ans) {
+    // Base condition: stop if index is equal to the length of the string
+    if (index == str.length()) {
+        return;
+    }
+    // Check if the current character is the target
+    if (str[index] == target) {
+        ans = index;
+    }
+    // Recurse to the next character
+    lastOccurance(str, target, index + 1, ans);
+}
+
+int main() {
+    string str = "abcddedg";
+    char target = 'd';
+
+    int ans = -1;
+    int index = 0;
+
+    lastOccurance(str, target, index, ans);
+    cout << ans;
+
+    return 0;
+}
