317b4933d0997db6b405e61c3ac512409b13dd36
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-30 18:05:05 +0530

    commit

---
+++ recursion/findMinimumSum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+int solve(vector<int>&arr,int target){
+    if (target==0)
+    {
+       return 0;
+    }
+    if (target<0){
+        return INT_MAX;
+    }
+    // to handle one case we have to make logic
+    int mini=INT_MAX;
+
+    for (int i = 0; i <arr.size(); i++)
+    {
+        int ans = solve(arr,target-arr[i]);
+        if (ans!=INT_MAX){
+            mini=min(mini,ans+1);
+        }
+    }
+    return mini; 
+}
+int main(){
+    vector<int>arr{1,2};
+    int target=5;
+    int ans=solve(arr,target);
+    cout<<ans;
+    return 0;
+}
