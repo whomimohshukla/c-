fbb3b38d056e30adfe188031db4d34c0b7fa1ca5
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-02 02:28:15 +0530

    commit

--- sortingAlgorithm/howTo.cpp
+++ sortingAlgorithm/howTo.cpp
@@ -11,21 +11,61 @@ void merge(int* arr,int start,int end){
     // creating one array (dynamic array) of length length2 and named right
     int*right=new int[length2];
 
-    //copying values
+    //copying values of left array(unsorted) and right(unsorted) in new array left and right 
+
 
     int k=start;
     for(int i=0;i<length1;i++){
         left[i]=arr[k];
         k++;
     }
+    k=mid+1;
+    for(int i=0;i<length2;i++){
+        right[i]=arr[k];
+        k++;
+    }
+
+
+    // merge to sorted array in one single sorted array
+    int leftIndex=0;
+    int rightIndex=0;
+    int arrIndex=start;
+
+    while (leftIndex<length1 && rightIndex<length2)
+    {
+        if(left[leftIndex]<=right[rightIndex]){
+            arr[arrIndex++]=left[leftIndex++];
+           
+        }
+        else{
+            arr[arrIndex++]=right[rightIndex++]; 
+        }
+       
+    }
+
+    //copy logic for left array
+    while(leftIndex<length1){
+        arr[arrIndex++]=left[leftIndex++];
+    }
+    
+    //copy logic for right array
+    while(rightIndex<length2){
+        arr[arrIndex++]=right[rightIndex++];
+    }
+    //delete dynamically created array
+    delete []left;
+    delete []right;
+
+    
+    
 
 
 
-}
-int mergeSOrt(int* arr,int start,int end){
+
+}void mergeSOrt(int* arr,int start,int end){
     // base case 
     if(start>=end){
-        return 0;
+        return ;
     }
     int mid=(start+end)/2;
 
@@ -45,6 +85,13 @@ int main(){
     int end=n-1;
 
     mergeSOrt(arr,start,end);
+
+    for (int i = 0; i <n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
 
     return 0;
 }
