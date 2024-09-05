658cbfdffd46ead271d72d753113dd50aca694c5
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-06 00:33:29 +0530

    commit

---
+++ sortingAlgorithm/selection/yes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        // Find the minimum element in the unsorted portion of the array
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element of the unsorted portion
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    cout << "Original array: ";
+    printArray(arr);
+
+    selectionSort(arr);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
