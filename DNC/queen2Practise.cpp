fe3894ef07a8f8a951e760f146db642ef31daf93
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-06 14:43:17 +0530

    commit Dnc

--- DNC/queen2Practise.cpp
+++ DNC/queen2Practise.cpp
@@ -17,6 +17,43 @@ void printSolution( vector<vector<int>> &board,int n){
      cout << endl;
 }
 
+bool isSafe(int row,int  column,  vector<vector<int>> &board, int n){
+
+    // left check
+    int i=row;
+    int j=column;
+    
+
+    // Check row on the left side
+    while (j >= 0) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+        j--;
+    }
+    // Check upper diagonal on the left side
+     // Check upper left diagonal
+    i = row;
+    j = column;
+    while (i >= 0 && j >= 0) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+        i--;
+        j--;
+    }
+    // Check lower diagonal on the left side
+    i = row;
+    j = column;
+    while (i < n && j >= 0) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
 void solve( vector<vector<int>> &board,int column,int n){
     // base case 
     if (column>=n)
@@ -26,6 +63,14 @@ void solve( vector<vector<int>> &board,int column,int n){
     }
     
     // one case 
+     // Try placing the queen in all rows one by one
+    for (int row = 0; row < n; row++) {
+        if (isSafe(row, column, board, n)) {
+            board[row][column] = 1;  // Place the queen
+            solve(board, column + 1, n);  // Recur to place rest of the queens
+            board[row][column] = 0;  // Backtrack and remove the queen
+        }
+    }
 
 }
 int main(){
