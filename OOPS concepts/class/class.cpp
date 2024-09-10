40369096586c6256f4a188129755469599428de6
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-10 23:27:33 +0530

    commit

---
+++ OOPS concepts/class/class.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std;
+// class cars{
+// };
+// int main(){
+//     cout<<sizeof(cars); >>>>>>>>>>>>>output 1
+//     return 0;
+// }
+
+
+
+
+#include<iostream>
+using namespace std;
+class cars{
+    int milage;
+    int diesel;
+    char parts;   //>>...this is due to padding and  greedy alignment output >>12;
+
+};
+int main(){
+    cout<<sizeof(cars);
+    return 0;
+}
+
+
+
+
+
+
+
