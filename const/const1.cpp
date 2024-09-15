7df384b7ef6df676358f3e63ea63ca6332635915
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-16 03:34:42 +0530

    commit

---
+++ const/const1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // const int x =5;
+  // // but we can not reasign a value
+  // // x = 10;
+  // cout<<"value of x is "<<x<<endl;
+  // // 2>>> const with pointers
+  // const int *a=new int(2);
+  // const data ,non-const pointer
+  //   int const *a = new int(2);
+  //   cout<<*a<<endl;
+  //   // same as line no 9
+  //   // cout<<*a;
+  //   // *a=20;// cannot change the value
+  //   int b = 20;
+  //   a = &b;
+  //   // pointer iteself can be assinged
+  //   cout << *a << endl;
+
+  // const pointer ,but non constant data
+  //   int *const a = new int(2);
+  //   cout << *a << endl;
+
+  //   // pointer iteself can be changed
+  //   *a = 20;
+  //   cout << *a << endl;
+  //   int b = 10;
+  //   //   a=&b; nhi chalega
+
+  // const value and const pointer
+  
+
+  return 0;
+}
