65b3f4393450e1fcf222e8b3bb7177750be6af90
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-21 22:20:18 +0530

    commit

---
+++ searching/2dsearch2p.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool searchMatrix(vector<vector<int>>& matrix, int target){
+    
+    int start=matrix.size();
+}
+
+int main(){
+    
+    vector<vector<int>> matrix{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int target=8;
+    bool ans= searchMatrix( matrix, target);
+
+    if (ans)
+    {
+        cout << "Element is present in the matrix" << endl;
+    }
+    else{
+        cout << "Element is not present in the matrix" << endl;
+    }
+
+    
+   
+    return 0;
+}
