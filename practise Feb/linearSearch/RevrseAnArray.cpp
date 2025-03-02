1044d9a90fb14037814cbcc06ff8c19e4d7c51eb
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-03-02 13:08:52 +0530

    linearSearch

---
+++ practise Feb/linearSearch/RevrseAnArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={10,30,40,50,60,70,80,90};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int start=0;
+    int end=size-1;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+ return 0;
+}
