d5ca7389ce2aa15ad0190df1f5354c1819c5b142
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-04-30 23:18:14 +0530

    searching and insertion in bst

---
+++ BST dsa/searching.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *search(Node *root, int data)
+{
+    if (root == NULL || root->data == data)
+    {
+        return root;
+    }
+
+    if (data < root->data)
+    {
+        return search(root->left, data);
+    }
+    else
+    {
+        return search(root->right, data);
+    }
+}
+
+Node *insert(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return new Node(data);
+    }
+
+    if (data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    else
+    {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    int key = 60;
+    Node *result = search(root, key);
+    if (result != NULL)
+    {
+        cout << "Found: " << result->data << endl;
+    }
+    else
+    {
+        cout << "Not Found" << endl;
+    }
+
+    return 0;
+}
