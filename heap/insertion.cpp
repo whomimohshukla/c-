a307c3e097b680fdc2f876d39d491c02005e6be9
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-05-04 20:25:26 +0530

    insertion in heap

--- heap/insertion.cpp
+++ heap/insertion.cpp
@@ -3,41 +3,93 @@
 #include <algorithm>
 using namespace std;
 
-class Heap {
+class Heap
+{
 public:
-    int arr[101];  // 1-based indexing
+    int arr[101]; // 1-based indexing
     int size;
 
-    Heap() {
+    Heap()
+    {
         size = 0;
         arr[0] = -1; // Optional: dummy value at index 0
     }
 
-    void insert(int value) {
+    void insert(int value)
+    {
         size += 1;
         int index = size;
         arr[index] = value;
 
-        while (index > 1) {
+        while (index > 1)
+        {
             int parentIndex = index / 2;
-            if (arr[index] > arr[parentIndex]) {
+            if (arr[index] > arr[parentIndex])
+            {
                 swap(arr[index], arr[parentIndex]);
                 index = parentIndex;
-            } else {
+            }
+            else
+            {
                 break;
             }
         }
     }
 
-    void print() {
-        for (int i = 1; i <= size; ++i) {
+    void deleteFromHeap()
+    {
+        if (size == 0)
+        {
+            cout << "Heap is empty!" << endl;
+            return;
+        }
+
+        // Step 1: Replace root with last element
+        arr[1] = arr[size];
+        size--;
+
+        // Step 2: Heapify-down from root
+        int i = 1;
+        while (i <= size)
+        {
+            int left = 2 * i;
+            int right = 2 * i + 1;
+            int largest = i;
+
+            if (left <= size && arr[left] > arr[largest])
+            {
+                largest = left;
+            }
+
+            if (right <= size && arr[right] > arr[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                swap(arr[i], arr[largest]);
+                i = largest;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; ++i)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
     }
 };
 
-int main() {
+int main()
+{
     Heap h;
     h.insert(50);
     h.insert(30);
@@ -47,5 +99,9 @@ int main() {
     h.insert(60);
 
     cout << "Max Heap: ";
-    h.print(); // Expected output: 60 30 50 10 5 40
+    h.print(); // Output should show a Max-Heap
+
+    cout << "After deleting root: ";
+    h.deleteFromHeap();
+    h.print(); // Root (max element) should be removed
 }
